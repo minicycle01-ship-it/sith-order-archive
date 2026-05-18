38be166eb2bfbe7efb3786a1333943b1cfb52454
minicycle01-ship-it <minicycle0.1@gmail.com>
2026-05-18 18:48:55 +0100

    Update data.js

--- data.js
+++ data.js
@@ -4,123 +4,12 @@ window.SITH_ORDER_DATA = {
       name: 'The Sith Vanguard',
       summary: 'Oversees battlefield readiness, tactics, and coordinated raid action.',
       purpose:
-        'The Sith Vanguard oversees the training of the Order’s tacticians. It is also responsible for organizing offensive or defensive raids with the Jedi.',
+        'The Sith Vanguard oversees the training of the Order\u2019s tacticians. It is also responsible for organizing offensive or defensive raids with the Jedi.',
       focus: 'Tactics, raids, battlefield readiness, operational leadership',
       atmosphere: 'Commanding, aggressive, disciplined'
     },
     {
       name: 'The Sith Doctrine',
       summary: 'Trains rising members and safeguards the teachings of the Order.',
-      purpose:
-        'The Sith Doctrine is tasked with the training of the Sith Hopefuls, Sith Acolytes, and aiding Apprentices with giving the basic knowledge that they need throughout their career in the Order. They also manage the Archives.',
-      focus: 'Instruction, doctrine, archives, member development',
-      atmosphere: 'Learned, severe, uncompromising'
-    },
-    {
-      name: 'Laws & Justice',
-      summary: 'Maintains internal order and oversees the judicial system of the Order.',
-      purpose:
-        'Primarily concerned with maintaining order and presiding over the judicial system, Laws & Justice is vital for the survival of the Order. It passes legal rulings based on the Sith Edict and Sith Code.',
-      focus: 'Law, order, rulings, oversight, discipline',
-      atmosphere: 'Authoritative, exacting, controlled'
-    },
-    {
-      name: 'The Dark Honor Guard',
-      summary: 'Protects the highest authorities and the sacred places of the Order.',
-      purpose:
-        'The Dark Honor Guard are entrusted and tasked with the protection of the Emperor, his councillors, and Sith places in whole.',
-      focus: 'Protection, ceremonial authority, security, loyalty',
-      atmosphere: 'Imposing, loyal, elite'
-    },
-    {
-      name: 'Scientific Advancement',
-      summary: 'Develops the technology and systems that support the Order on Roblox.',
-      purpose:
-        'Charged with the technology innovation of the Order, Scientific Advancement houses the developers that make the Order possible on Roblox. They work closely with the Jedi Artisans to ensure both Orders are up to date on the community’s technology.',
-      focus: 'Technology, development, systems, innovation',
-      atmosphere: 'Analytical, progressive, secretive'
-    }
-  ],
-  links: [
-    {
-      title: 'The Sith Code',
-      description: 'The Sith Code Trello board contains all information about the Order in one place. It is mandatory to read all cards labeled with the green marker titled "Essential."',
-      href: 'https://trello.com/b/5o48rAhZ/the-sith-order-sith-code'
-    },
-    {
-      title: 'The Sith Edict',
-      description: 'The Sith Edict outlines the Sith Order’s rules, punishments, and procedures. Reading this document is required by all members of the Order.',
-      href: 'https://docs.google.com/document/d/1HsswQNCc5E0N1EaV_BMUOjaoWgOkcfxm3dFY29md-FQ/edit?usp=sharing'
-    },
-    {
-      title: 'Sith High Command',
-      description: 'A guideline area for Sith High Command members and a public reference for the duties and tasks carried by High Command.',
-      href: 'https://docs.google.com/document/d/1UZOfIejOzWQR2XP9xfNMkjuhTWwycjWyLYyuWDgUjQU/edit'
-    },
-    {
-      title: 'Orion Community Discord Server',
-      description: 'A shared community server where members of both Jedi and Sith can convene for raids, gamenights, and special events.',
-      href: 'https://discord.gg/WxKkancgZ8'
-    },
-    {
-      title: 'Kaggath Regulations',
-      description: 'Outlines the statutes for conducting a Kaggath, including current holders and relics obtainable through Kaggaths.',
-      href: 'https://docs.google.com/document/d/1H-Q3JWjgIwiJ5s6eTYYZrViBOIbo_NkLH0FEGLISARs/edit?tab=t.0'
-    },
-    {
-      title: 'Rank Progression Registry',
-      description: 'Track progression and rank development within the Order.',
-      href: 'https://docs.google.com/spreadsheets/d/1gBYti4lZ28jnAKjnyXuH9Pw_FowA9zLwcSZe03GFL5w/edit?usp=sharing'
-    },
-    {
-      title: 'Assassination Regulations',
-      description: 'Any information regarding assassinations will be found here. This document outlines the regulations governing them.',
-      href: 'https://docs.google.com/document/d/1HOiq5PCVOjFrFsKIRGdteO_tFO0OhdHWTDVhsKpApmo/edit?usp=sharing'
-    },
-    {
-      title: 'Assassination Board',
-      description: 'The Sith Order assassination board. The board resets on the first of every month. Targets get cleared once marked after two days of their original time becoming marked.',
-      href: 'https://docs.google.com/spreadsheets/d/1yAex-PT2zCq9i75AV92N0Z9AtLOHStUd3iC9lIld_Nc/edit?usp=sharing'
-    },
-    {
-      title: 'Ashas Ree',
-      description: 'Sith territory.',
-      href: 'https://www.roblox.com/games/16012495353/The-Sith-Temple-on-Ashas-Ree'
-    },
-    {
-      title: 'Malachor',
-      description: 'Sith territory.',
-      href: 'https://www.roblox.com/games/17120897870/Malachor'
-    },
-    {
-      title: 'Naboo',
-      description: 'Jedi territory.',
-      href: 'https://www.roblox.com/games/6112010593/--'
-    },
-    {
-      title: 'Toola',
-      description: 'Battleground.',
-      href: 'https://www.roblox.com/games/16092485546/Toola-Battlegrounds'
-    },
-    {
-      title: 'Shu Torun',
-      description: 'Battleground.',
-      href: 'https://www.roblox.com/games/16092454069/Shu-Torun-Battlegrounds'
-    },
-    {
-      title: 'Panna Prime',
-      description: 'Battleground.',
-      href: 'https://www.roblox.com/games/16092468118/Panna-Prime-Battlegrounds'
-    },
-    {
-      title: 'Balmorra',
-      description: 'Battleground.',
-      href: 'https://www.roblox.com/games/16092503821/Balmorra-Battlegrounds'
-    },
-    {
-      title: 'Clothing Couturier',
-      description: 'Server link.',
-      href: 'https://discord.gg/dNhhpkqTeW'
-    }
-  ]
-};
+      
+...(truncated)...
